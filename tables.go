cf51fc67f44e447e4fbfda9dc6da2e527d44b937
bokwoon <bokwoon.c@gmail.com>
2022-07-24 22:27:28 +0800

    checkpoint

--- tables.go
+++ tables.go
@@ -11,16 +11,31 @@ type PM_SITE struct {
 	IS_PRIMARY     sq.BooleanField `ddl:"unique"`
 }
 
+type PM_PLUGIN struct {
+	sq.TableStruct
+	PLUGIN        sq.StringField `ddl:"primarykey"`
+	CONFIG        sq.JSONField
+	CONFIG_SCHEMA sq.JSONField
+}
+
+type PM_HANDLER struct {
+	sq.TableStruct `ddl:"primarykey=plugin,handler"`
+	PLUGIN         sq.StringField `ddl:"references=pm_plugin"`
+	HANDLER        sq.StringField
+	CONFIG_SCHEMA  sq.JSONField
+}
+
 type PM_ROUTE struct {
 	sq.TableStruct  `ddl:"unique=site_id,path"`
 	SITE_ID         sq.UUIDField
 	ROUTE_ID        sq.UUIDField `ddl:"primarykey"`
 	PARENT_ROUTE_ID sq.UUIDField `ddl:"references={pm_route.route_id index}"`
 	BASENAME        sq.StringField
+	PATH            sq.StringField `ddl:"index"`
 	PLUGIN          sq.StringField
 	HANDLER         sq.StringField
-	DATA            sq.JSONField
-	PATH            sq.StringField `ddl:"index"`
+	CONFIG          sq.JSONField
+	_               struct{} `ddl:"foreignkey={plugin,handler references=pm_handler}"`
 }
 
 type PM_ROUTE_HIERARCHY struct {

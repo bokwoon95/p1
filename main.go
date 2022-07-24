05ef0dc095ae8adc75f7a3b0710fb8d1feaae588
bokwoon <bokwoon.c@gmail.com>
2022-07-25 01:44:52 +0800

    checkpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package pagemanager
 import (
 	"database/sql"
 	"io/fs"
+	"net/http"
 )
 
 type Pagemanager struct {
@@ -12,8 +13,23 @@ type Pagemanager struct {
 	db      *sql.DB
 }
 
+// The only 3 dependencies the pagemanager will inject each handler with: fsys fs.FS, db *sql.DB, config []byte.
+
+// Won't they want to know the SiteID, RouteID and UserID as well?
+
+// When registering plugins, each plugin must provide an fs.FS containing the JSON schema files for each handler followed by a map[string]func(fsys fs.FS, db *sql.DB, config []byte) http.Handler
+
+// The problem with bundling config and data together is that when the user switches to a different template the data form cannot change accordingly.
+
 type PageRenderer struct {
 	fsys     fs.FS
 	filename string
 	config   []byte
+}
+
+func (pr *PageRenderer) HandlerName() string {
+	return ""
+}
+
+func (pr *PageRenderer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
--- tables.go
+++ tables.go
@@ -16,9 +16,19 @@ type PM_SITE struct {
 // plugins and handlers exist and have been registered. No need for foreign
 // keys.
 
+// TODO: combine plugin+tag and plugin+role together so that I don't have to
+// keep lugging around a reference to plugin everytime I need to reference a
+// tag or role.
+
+// TODO: what exactly does a handler receive?
+// Handler receives the current user and his permissions on the current route
+// (all automatically calculated through the combination of
+// user+roles+route+tags). All the handler has to check is if the permission it
+// is interested in is present in the permission list.
+
 type PM_ROUTE struct {
 	sq.TableStruct  `ddl:"unique=site_id,path"`
-	SITE_ID         sq.UUIDField
+	SITE_ID         sq.UUIDField `ddl:"notnull references={pm_site index}"`
 	ROUTE_ID        sq.UUIDField `ddl:"primarykey"`
 	PARENT_ROUTE_ID sq.UUIDField `ddl:"references={pm_route.route_id index}"`
 	BASENAME        sq.StringField
@@ -34,4 +44,86 @@ type PM_ROUTE_HIERARCHY struct {
 	ROUTE_ID          sq.UUIDField `ddl:"references=pm_route.route_id"`
 	ANCESTOR_ROUTE_ID sq.UUIDField `ddl:"references={pm_route.route_id index}"`
 	DEPTH             sq.NumberField
+}
+
+type PM_ROLE struct {
+	sq.TableStruct `ddl:"primarykey=site_id,plugin,role"`
+	SITE_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	ROLE           sq.StringField
+}
+
+type PM_TAG struct {
+	sq.TableStruct `ddl:"primarykey=site_id,plugin,tag"`
+	SITE_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	TAG            sq.StringField
+}
+
+type PM_ROLE_PERMISSION struct {
+	sq.TableStruct `ddl:"primarykey=site_id,plugin,role,permission"`
+	SITE_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	ROLE           sq.StringField
+	PERMISSION     sq.StringField
+}
+
+type PM_TAG_PERMISSION struct {
+	sq.TableStruct `ddl:"primarykey=site_id,plugin,tag,permission"`
+	SITE_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	TAG            sq.StringField
+	PERMISSION     sq.StringField
+}
+
+type PM_TAG_OWNER struct {
+	sq.TableStruct `ddl:"primarykey=site_id,plugin,tag,role"`
+	SITE_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	TAG            sq.StringField
+	ROLE           sq.StringField
+}
+
+type PM_USER struct {
+	sq.TableStruct         `ddl:"unique=site_id,username unique=site_id,email"`
+	SITE_ID                sq.UUIDField `ddl:"notnull references={pm_site index}"`
+	USER_ID                sq.UUIDField `ddl:"primarykey"`
+	NAME                   sq.StringField
+	USERNAME               sq.StringField `ddl:"notnull"`
+	EMAIL                  sq.StringField
+	PASSWORD_HASH          sq.StringField
+	RESET_PASSWORD_TOKEN   sq.StringField
+	RESET_PASSWORD_SENT_AT sq.TimeField
+}
+
+type PM_SESSION struct {
+	sq.TableStruct
+	SESSION_HASH sq.BinaryField
+	USER_ID      sq.UUIDField `ddl:"references=pm_user"`
+}
+
+type PM_USER_ROLE struct {
+	sq.TableStruct `ddl:"primarykey=user_id,plugin,role"`
+	SITE_ID        sq.UUIDField `ddl:"notnull references={pm_site index}"`
+	USER_ID        sq.UUIDField
+	PLUGIN         sq.StringField
+	ROLE           sq.StringField
+	_              struct{} `ddl:"foreignkey={site_id,plugin,role reference=pm_role}"`
+}
+
+type PM_ROUTE_ROLE_PERMISSION struct {
+	sq.TableStruct `ddl:"primarykey=route_id,plugin,role,permission"`
+	SITE_ID        sq.UUIDField `ddl:"notnull references={pm_site index}"`
+	ROUTE_ID       sq.UUIDField `ddl:"notnull references=pm_route"`
+	PLUGIN         sq.StringField
+	ROLE           sq.StringField
+	PERMISSION     sq.StringField
+}
+
+type PM_ROUTE_TAG struct {
+	sq.TableStruct `ddl:"primarykey=route_id,plugin,tag"`
+	SITE_ID        sq.UUIDField `ddl:"notnull references={pm_site index}"`
+	ROUTE_ID       sq.UUIDField `ddl:"notnull references=pm_route"`
+	PLUGIN         sq.StringField
+	TAG            sq.StringField
 }
